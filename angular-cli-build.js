7d249bc34134251197f187c6271957990596db35
Luke Johnstone <luke@touchfoundry.co.za>
2016-08-16 17:28:25 +0200

    ADDED:   #updated material import

--- angular-cli-build.js
+++ angular-cli-build.js
@@ -24,7 +24,7 @@ module.exports = function(defaults) {
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
       '@angular/**/*.+(js|js.map)',
-      'material-design-lite/dist/**/*.min.+(js|js.map)'
+      'material-design-lite/*.min.+(js|js.map)'
     ]
   });
 };
